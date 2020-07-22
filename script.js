50506a27099e82d85fa24e21d0c1ca67d6dfe21f
Kassi Stumb <kassi.mcintosh@gmail.com>
2020-07-22 16:52:28 -0400

    css updates, js functional

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
+  var events = [];
+  
   // listen for save button clicks
-
   $(".saveBtn").on("click", function() {
     // get nearby values
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
-    console.log('value:', value);
-    console.log('time:', time);
+    events.push({description: value, time: time});
 
     // save the value in localStorage as time
+    localStorage.setItem("events", JSON.stringify(events));
     
   });
 
@@ -18,22 +19,38 @@ $(document).ready(function() {
     var currentHour = moment().hours();
     console.log('current hour:', currentHour);
 
+    // clear day
+    if(currentHour > 23) {
+      for(var i = 0; i < events.length; i++) {
+      events[i].description = "";
+      events[i].time = "";
+      events.length = 0;
+      }
+    }
+
     // loop over time blocks
     $(".time-block").each(function() {
       var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-      console.log("block hour:", blockHour);
-
       // check if we've moved past this time
-      
       // if the current hour is greater than the block hour
       // then add class "past"
-
+      if(currentHour > blockHour) {
+        $(this).addClass("past");
+      }
       // if they are equal
       // then remove class "past" and add class "present"
-
+      else if(currentHour === blockHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      }
       // else
       // remove class "past", remove class "present", add class "future"
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
       
     });
   }
@@ -42,9 +59,32 @@ $(document).ready(function() {
 
   // set up interval to check if current time needs to be updated
   // which means execute hourUpdater function every 15 seconds
+  var secondsLeft = 15;
+  function setTime() {
+    setInterval(function() {
+      secondsLeft--;
+  
+      if(secondsLeft === 0) {
+        hourUpdater();
+        secondsLeft = 15;
+      }
+  
+    }, 1000);
+  }
+  setTime();
 
   // load any saved data from localStorage
-  
+  var storedEvents = JSON.parse(localStorage.getItem("events"));
+
+  if (storedEvents !== null) {
+    events = storedEvents;
+  }
+
+  for(var i = 0; i < events.length; i++) {
+    var userDescription = events[i].description;
+    $("#" + events[i].time).children(".description").text(userDescription);
+  }
+
 
   // display current day on page
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
